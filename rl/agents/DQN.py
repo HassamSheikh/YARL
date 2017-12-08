db9128e3ea6673b5af0c33a7eb41451c07b69737
Hassam Ullah Sheikh <hassamsheikh1@gmail.com>
2017-12-08 18:38:13 -0500

    Adding Polyak Averaging to DQN

--- rl/agents/DQN.py
+++ rl/agents/DQN.py
@@ -4,7 +4,7 @@ from rl.util import *
 from keras.optimizers import Adam
 
 class DQNAgent:
-    def __init__(self, env, model, gamma, replay_buffer, policy):
+    def __init__(self, env, model, gamma, replay_buffer, policy, tau):
         self.env=env
         self.trainable_model=model
         self.replay_buffer=replay_buffer
@@ -12,20 +12,24 @@ class DQNAgent:
         self.gamma=gamma
         self.state_dim=env.observation_space.shape[0]
         self.action_dim=env.action_space.n
+        self.tau=tau
         self.target_model=0
 
     def train_model(self, training_data, training_label):
         self.trainable_model.train_on_batch(training_data, training_label)
 
     def update_target_model(self):
-        self.target_model.set_weights(self.trainable_model.get_weights())
+        if self.tau>1:
+            self.target_model=update_model_by_polyak_average(self.target_model, self.trainable_model, self.tau)
+        else:
+            self.target_model=clone_weights(self.target_model, self.trainable_model)
 
     def compute_q_values(self, states, target=False):
         if (states.shape)==(self.state_dim,):
-            return self.trainable_model.predict(states.reshape(1, self.state_dim)).flatten()
+            return self.trainable_model.predict(states.reshape(1, self.state_dim)).flatten() #Predicting Q values of a single state
         if target:
-            return self.target_model.predict(states)
-        return self.trainable_model.predict(states)
+            return self.target_model.predict(states) #Querying target network for Q values of multiple states
+        return self.trainable_model.predict(states) #Querying the trainable network for Q values of multiple states
 
     def experience_replay(self, experiences):
         states, actions, rewards, next_states=zip(*[[experience[0], experience[1], experience[2], experience[3]] for experience in experiences])
@@ -49,25 +53,26 @@ class DQNAgent:
 
 
     def fit(self, number_of_epsiodes, batch_size):
-        for episode in generator(number_of_epsiodes):
-            total_reward=0
-            state=self.env.reset()
-            if episode%100==0:
+        for episode in generator(number_of_epsiodes): #Looping through total number of episodes
+            total_reward=0 #The total reward an agent will get after an epsiode
+            state=self.env.reset() #Resetting environment
+            if self.tau>1:
+                self.update_target_model() #Update the target network using Polyak averaging
+            if episode%5000==0:
                 print("Updating Target Model")
-                self.update_target_model()
+                self.update_target_model() #Update the target network after N episodes
             while True:
-                #self.env.render()
-                q_values_for_state=self.compute_q_values(state)
-                action=self.policy.select_action(q_values_for_state)
+                #self.env.render()  #Rendering the environment
+                q_values_for_state=self.compute_q_values(state) #Compute Q value for the current state
+                action=self.policy.select_action(q_values_for_state) #Select action based on the policy
                 next_state, reward, done, _=self.env.step(action)
                 if done:
-                    next_state = None
-                self.replay_buffer.add_to_buffer([state, action, reward, next_state])
-                state = next_state
-                total_reward += reward
-                if len(self.replay_buffer.buffer) > 50:
-                    experience=self.replay_buffer.sample(batch_size)
-                    self.experience_replay(experience)
+                    next_state=None #If the next state is terminal, mark it as None
+                self.replay_buffer.add_to_buffer([state, action, reward, next_state]) #Adding experience to replay buffer
+                state=next_state
+                total_reward +=reward
+                if len(self.replay_buffer.buffer) > 50:  #Warming Up
+                    experience=self.replay_buffer.sample(batch_size) #Sampling from replay buffer
+                    self.experience_replay(experience) #Experience replay step
                 if done:
                     break
-            print("Total reward:", total_reward)
