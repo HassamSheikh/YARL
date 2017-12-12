5014e8a2bea564c61379389162922a29fe595507
Hassam Ullah Sheikh <hassamsheikh1@gmail.com>
2017-12-12 11:00:38 -0500

    Polyak Average added

--- examples/dqn_cartpole.py
+++ examples/dqn_cartpole.py
@@ -13,13 +13,12 @@ def create_model(state_dim, number_of_actions):
     model.add(Dense(output_dim=64, activation='relu', input_dim=state_dim))
     model.add(Dense(output_dim=number_of_actions, activation='linear'))
     opt = RMSprop(lr=0.00025)
-    model.compile(loss='mse', optimizer=opt)
     return model
 
 env = gym.make('CartPole-v0')
 model=create_model(4, env.action_space.n)
 replay_buffer=ReplayBuffer()
 policy=DecayingEpsilonGreedyPolicy()
-agent=DQNAgent(env, model, 0.99, replay_buffer, policy, 0)
+agent=DQNAgent(env, model, policy, replay_buffer, tau=0.99)
 agent.compile()
-agent.fit(1000, 64)
+agent.fit(10000)
--- rl/agents/DQN.py
+++ rl/agents/DQN.py
@@ -1,23 +1,31 @@
 import random
+import warnings
 import numpy as np
 from rl.util import *
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSprop
 
 class DQNAgent:
-    def __init__(self, env, model, gamma, replay_buffer, policy, tau):
+    def __init__(self, env, model, policy, replay_buffer, gamma=0.99, batch_size=64, tau=0, target_model_update_interval=10000, render=False):
         self.env=env
         self.trainable_model=model
         self.target_model=model
-        self.replay_buffer=replay_buffer
         self.policy=policy
+        self.replay_buffer=replay_buffer
         self.gamma=gamma
+        self.batch_size=batch_size
+        self.tau=tau
+        self.render=render
+        self.target_model_update_interval=target_model_update_interval
         self.state_dim=env.observation_space.shape[0]
         self.action_dim=env.action_space.n
-        self.tau=tau
 
+        if self.render:
+            warnings.warn("Warning: Rendering environment will make the training extremely slow")
+        if self.tau>0:
+            warnings.warn("Warning: Polyvak Averaging will be used to update target model at every step")
 
-    def train_model(self, training_data, training_label):
-        self.trainable_model.fit(training_data, training_label, batch_size=64, verbose=0) #Training the network
+    def train_model(self, training_data, training_label, batch_size=64):
+        self.trainable_model.fit(training_data, training_label, batch_size=self.batch_size, verbose=0) #Training the network
 
     def update_target_model(self):
         if self.tau>0:
@@ -37,39 +45,13 @@ class DQNAgent:
         return self.trainable_model.predict(states) #Querying the trainable network for Q values of multiple states
 
     def experience_replay(self):
-        batch=self.replay_buffer.sample(64) #Sampling from replay buffer
-        batchLen = len(batch)
-
-        no_state = np.zeros(self.state_dim)
-
-        states = np.array([ o[0] for o in batch ])
-        states_ = np.array([ (no_state if o[3] is None else o[3]) for o in batch ])
-
-        p = self.compute_q_values(states)
-        p_ = self.compute_q_values(states_)
-
-        new_state = np.zeros((batchLen, self.state_dim))
-        new_value = np.zeros((batchLen, self.action_dim))
-        for i in range(batchLen):
-            o = batch[i]
-            s = o[0]; a = o[1]; r = o[2]; s_ = o[3]
-
-            t = p[i]
-            if s_ is None:
-                t[a] = r
-            else:
-                t[a] = r + self.gamma * np.amax(p_[i])
-
-            new_state[i] = s
-            new_value[i] = t
-
-        experiences=batch #Sampling from replay buffer
+        experiences = self.replay_buffer.sample(self.batch_size)
         states, actions, rewards, next_states=zip(*[[experience[0], experience[1], experience[2], experience[3]] for experience in experiences]) #Seperating states, actions, rewards and next states
         states=np.asarray(states) #Converting to numpy array
         place_holder_state=np.zeros(self.state_dim)
         next_states_=np.asarray([(place_holder_state if next_state is None else next_state) for next_state in next_states]) #Converting to numpy array
         q_values_for_states=self.compute_q_values(states)
-        q_values_for_next_states=self.compute_q_values(next_states_, target=False) #Computing the max Q(S',A') for the using the target network
+        q_values_for_next_states=self.compute_q_values(next_states_, True) #Computing the max Q(S',A') for the using the target network
         for x in range(len(experiences)):
             y_true=rewards[x]
             if next_states[x] is not None:
@@ -78,32 +60,32 @@ class DQNAgent:
         self.train_model(states, q_values_for_states)
 
 
-    def compile(self, opt=Adam(lr=0.001)):
-        pass
-        # self.target_model=clone_model(self.trainable_model)
-        # self.trainable_model.compile(loss='mse', optimizer=opt)
-        # self.target_model.compile(loss='mse', optimizer=opt)
+    def compile(self):
+        opt = RMSprop(lr=0.00025)
+        self.target_model=clone_model(self.trainable_model)
+        self.trainable_model.compile(loss='mse', optimizer=opt)
+        self.target_model.compile(loss='mse', optimizer=opt)
 
 
-    def fit(self, number_of_epsiodes, batch_size):
+    def fit(self, number_of_epsiodes):
         for episode in range(number_of_epsiodes): #Looping through total number of episodes
             total_reward=0 #The total reward an agent will get after an epsiode
             state=self.env.reset() #Resetting environment
-            # if self.tau<=0 and episode%5000==0:
-            #     print("Updating Target Model")
-            #     self.update_target_model() #Update the target network after N episodes
+
             while True:
-                #self.env.render()  #Rendering the environment
+                self.env.render() if self.render == True else False #Rendering the environment
                 action=self.select_action(state) #Select action based on the policy
                 next_state, reward, done, _=self.env.step(action)
                 if done:
                     next_state=None #If the next state is terminal, mark it as None
+
                 self.replay_buffer.add_to_buffer((state, action, reward, next_state)) #Adding experience to replay buffer
                 self.experience_replay() #Experience replay step
-
 
                 state=next_state
                 total_reward +=reward
                 if done:
                     break
+            if self.tau>0 or episode%self.target_model_update_interval==0:
+                self.update_target_model()
             print("Total reward ", total_reward)
